972ebdbf18e425a845a21dce00c07fec0504ed89
kevingtzz <kevingtzz@outlook.com>
2018-10-18 12:07:58 -0500

    Punto 1 finished

--- Punto1.cpp
+++ Punto1.cpp
@@ -1,29 +1,52 @@
 #include <cstdio>
 #include <algorithm>
 
-struct Estructure{
-  int index, Weight, Smart;
+class Elephant{
+  private:
+    int index, Weight, Smart;
     
-  Estructure() {} // Se define la estructura vacia en caso de no haber parametros.
-    
-  Estructure(int _index, int _Weight, int _Smart) { // Se define la estructura de elefantes que contiene el indice, su peso e inteligencia.
-    index = _index;
-    Weight = _Weight;
-    Smart = _Smart;
-  }
-    
-  bool operator < (Estructure X) const{ //Se sobrecarga el operador '<' para evaluar los atributos "Weight" y "Smart" de cada elefante.
-    if(Weight != X.Weight) return Weight < X.Weight;
-    return Smart > X.Smart;
-  }
+  public:
+    Elephant();
+    Elephant(int, int, int);
+    //~Elephant();
+
+    int get_index();
+    int get_Weight();
+    int get_Smart();
+
+    bool operator < (Elephant X) const{ //Se sobrecarga el operador '<' para evaluar los atributos "Weight" y "Smart" de cada elefante.
+      if(Weight != X.Weight) return Weight < X.Weight;
+      return Smart > X.Smart;
+    }
 };
+    
+Elephant::Elephant() {}
+    
+Elephant::Elephant(int _index, int _Weight, int _Smart) {
+  index = _index;
+  Weight = _Weight;
+  Smart = _Smart;
+}
+
+int Elephant::get_index() {
+  return index;
+}
+
+int Elephant::get_Weight() {
+  return Weight;
+}
+
+int Elephant::get_Smart() {
+  return Smart;
+}
+    
 
 int main(){
   int n = 0, Weight, Smart;
-  Estructure elephants[1000]; //Se inicializa un array de estructuras de elefantes con maximo 1000 elefantes
+  Elephant elephants[1000]; //Se inicializa un array de estructuras de elefantes con maximo 1000 elefantes
     
   while(scanf("%d %d", &Weight, &Smart) == 2) { //Mientras hayan dos datos de entrada se asignaran a una de las estructuras del arreglo. 
-    elephants[n] = Estructure(++n, Weight, Smart);
+    elephants[n] = Elephant(++n, Weight, Smart);
   }
 
   std::sort(elephants, elephants+n);//Teniendo en cuenta la sobrecarga efectuada en el operador  '<'  se organiza el arreglo con un sort
@@ -35,7 +58,7 @@ int main(){
     next[i] = -1;
         
     for(int j = i + 1; j < n; j++){
-      if(elephants[i].Weight < elephants[j].Weight && elephants[i].Smart > elephants[j].Smart && 1+dp[j] > dp[i]){
+      if(elephants[i].get_Weight() < elephants[j].get_Weight() && elephants[i].get_Smart() > elephants[j].get_Smart() && 1+dp[j] > dp[i]){
 	      dp[i] = 1 + dp[j];
 	      next[i] = j;
       }
@@ -48,7 +71,7 @@ int main(){
   }
     
   printf("%d\n", ans);
-  for(int i = start; i != -1; i = next[i]) printf("%d\n", elephants[i].index);
+  for(int i = start; i != -1; i = next[i]) printf("%d\n", elephants[i].get_index());
     
   return 0;
 }
